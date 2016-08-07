d851f9f4390f4d07111333ed16fd8529cdcdfaf5
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 13:36:02 -0700

    Imperative routing, ie injecting the router into the constructor and triggering the nav event programatically

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ROUTER_DIRECTIVES} from "@angular/router";
+import {ROUTER_DIRECTIVES, Router} from "@angular/router";
 
 @Component({
   moduleId: module.id,
@@ -10,9 +10,13 @@ import {ROUTER_DIRECTIVES} from "@angular/router";
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onNavigate() {
+    this.router.navigate(['/']);
   }
 
 }
