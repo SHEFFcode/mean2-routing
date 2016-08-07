bf5a03e9e5e3b6215a84bfbbe8550bab76cd9ca8
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 14:28:18 -0700

    child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {UserComponent} from "./user/user.component";
 import {HomeComponent} from "./home.component";
 import {provideRouter} from "@angular/router";
+import {USER_ROUTES} from "./user/user.routes";
 
 const APP_ROUTES = [
   {path: 'user/:id', component: UserComponent},
+  {path: 'user/:id', component: UserComponent, children: USER_ROUTES},
   {path: '', component: HomeComponent}
 ];
 
---
+++ src/app/user/user-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-user-detail',
+  templateUrl: 'user-detail.component.html',
+  styleUrls: ['user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
