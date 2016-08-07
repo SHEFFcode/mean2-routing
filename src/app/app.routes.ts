319c4ae50f75d2392dab3fc15a55bd55618ba256
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 14:37:42 -0700

    request rediraction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import {UserComponent} from "./user/user.component";
 import {HomeComponent} from "./home.component";
-import {provideRouter} from "@angular/router";
+import {provideRouter, RouterConfig} from "@angular/router";
 import {USER_ROUTES} from "./user/user.routes";
 
-const APP_ROUTES = [
+const APP_ROUTES: RouterConfig = [
   {path: 'user/:id', component: UserComponent},
   {path: 'user/:id', component: UserComponent, children: USER_ROUTES},
-  {path: '', component: HomeComponent}
+  {path: 'user', redirectTo: 'user/1', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  {path: '**', redirectTo: 'user/1', pathMatch: 'full'}
 ];
 
 export const APP_ROUTES_PROVIDER = [
