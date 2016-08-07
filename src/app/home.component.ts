5ffab4e6869a616ab3eae0aa8b4929b433658307
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 14:11:42 -0700

    adding and reading route params

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   moduleId: module.id,
@@ -6,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  param: string;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.subscription = router.routerState.queryParams.subscribe(
+      (queryParam: any) => this.param = queryParam['analytics']
+    );
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
