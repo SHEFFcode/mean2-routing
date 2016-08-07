aae42a238d432d2c61bb06749f3d1cc9f93784ea
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 13:31:04 -0700

    understand the relative vs absolute routing in angular 2'

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
   moduleId: module.id,
   selector: 'app-user',
   templateUrl: 'user.component.html',
-  styleUrls: ['user.component.css']
+  styleUrls: ['user.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class UserComponent implements OnInit {
 
