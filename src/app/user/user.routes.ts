92d05c103bd74cda79c6701b2cfb0748d4a56856
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 14:57:13 -0700

    Can Activate Guard

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,8 +1,9 @@
 import {RouterConfig} from "@angular/router";
 import {UserDetailComponent} from "./user-detail.component";
 import {UserEditComponent} from "./user-edit.component";
+import {UserDetailGuard} from "./user-detail.guard";
 
 export const USER_ROUTES: RouterConfig =  [
-  {path: 'detail', component: UserDetailComponent},
+  {path: 'detail', component: UserDetailComponent, canActivate: [UserDetailGuard]},
   {path: 'edit', component: UserEditComponent}
 ];
